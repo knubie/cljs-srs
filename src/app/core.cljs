60162d91743302293857bf24419b18116e3c8fc7
Matthew Steedman <steedman87@gmail.com>
2018-04-28 12:23:42 -0400

    Refactor

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -2,9 +2,7 @@
   (:require [reagent.core :as r]
             [app.styles   :as styles]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;  SCHEMA
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; -- Schema ------------------------------------------------------------------
 
 ; note-type
 ;   fields: [field-type]
@@ -27,99 +25,183 @@
 ;   card-type
 ;   status: :new | :learning | :to-review
 
-;(random-uuid)
+;; -- Default Seed Data -------------------------------------------------------
+
 (def state (r/atom {
   :actions []
   :ui {
     :workspace [:home]
   }
   :db {
-    :note-types {
-      :nt1 {:id :nt1 :name "Default Note" :field-types [:ft1 :ft2]}
-    }
-    :field-types {
-      :ft1 {:id :ft1 :name "Front" :note-type :nt1 :type "text" }
-      :ft2 {:id :ft1 :name "Back" :note-type :nt1 :type "text" }
-    }
-    :notes {
-      :n1 {:id :n1 :note-type :nt1 :fields [:f1 :f2]}
-      :n2 {:id :n2 :note-type :nt1 :fields [:f3 :f4]}
-    }
-    :fields {
-      :f1 {:id :f1 :field-type :ft1 :note :n1 :value "こんにちは"}
-      :f2 {:id :f2 :field-type :ft2 :note :n1 :value "Hello"}
-
-      :f3 {:id :f3 :field-type :ft1 :note :n2 :value "おはようございます"}
-      :f4 {:id :f4 :field-type :ft2 :note :n2 :value "Good morning"}
-    }
-    :card-types {
-      :ct1 {:id :ct1 :name "Default Card" :note-type :nt1 :sides [:s1 :s2]}
-    }
-    :sides {
-      :s1 {:id :s1 :name "Front" :display "{{Front}}"}
-      :s2 {:id :s2 :name "Back" :display "{{Back}}"}
-    }
-    :cards {
-      :c1 {:id :c1 :card-type :ct1 :note :n1 :deck :d1 :status :new :last-review "3-10-2018"}
-      :c2 {:id :c2 :card-type :ct1 :note :n2 :deck :d1 :status :new :last-review "3-10-2018"}
-      :c3 {:id :c3 :card-type :ct1 :note :n1 :deck :d2 :status :new :last-review "4-15-2018"}
-    }
-    :decks {
-      :d1 {:id :d1 :name "日本語"}
-      :d2 {:id :d2 :name "foobar"}
-    }
+    :note-types  { }
+    :field-types { }
+    :notes       { }
+    :fields      { }
+    :card-types  { }
+    :sides       { }
+    :cards       { }
+    :decks       { }
   }
 }))
 
-;; Models
-(defprotocol HasManyCards
-  (cards [self] "Returns associated cards."))
-
-(defrecord Deck [id name]
-
-  HasManyCards
-  (cards [self]
-    (->> @state :db :cards (where :deck (:id self)))))
-
-;; Queries
+(def db-note-types  (r/cursor state [:db :note-types]))
+(def db-field-types (r/cursor state [:db :field-types]))
+(def db-notes       (r/cursor state [:db :notes]))
+(def db-fields      (r/cursor state [:db :fields]))
+(def db-card-types  (r/cursor state [:db :card-types]))
+(def db-sides       (r/cursor state [:db :sides]))
+(def db-cards       (r/cursor state [:db :cards]))
+(def db-decks       (r/cursor state [:db :decks]))
+(def ui-workspace   (r/cursor state [:ui :workspace]))
 
 (defn add-deck! [name]
   (let [id (keyword (str (random-uuid)))]
   (swap! state assoc-in [:db :decks id] {:id id :name name})
   id))
 
-(add-deck! "My new deck")
+;; -- Seed Data ---------------------------------------------------------------
 
-;; Useful for iterators
-(defn get-record [[id record]] record)
-(defn get-id     [[id record]] id)
+(defn add-record! [model attrs]
+  (let [id (keyword (str (random-uuid)))]
+  (swap! state assoc-in [:db model id] (assoc attrs :id id))
+  id))
+
+(def default-note-type
+  (add-record! :note-types { :name "Default Note" }))
+
+(def question-field-type
+  (add-record! :field-types {
+    :name "Question"
+    :type "text" ;; TODO: Validate
+    :note-type default-note-type }))
+
+(def answer-field-type
+  (add-record! :field-types {
+    :name "Answer"
+    :type "text" ;; TODO: Validate
+    :note-type default-note-type }))
+
+(def note1
+  (add-record! :notes { :note-type default-note-type }))
+(def note2
+  (add-record! :notes { :note-type default-note-type }))
+
+(add-record! :fields {
+  :field-type question-field-type
+  :note note1
+  :value "こんにちは" })
+
+(add-record! :fields {
+  :field-type answer-field-type
+  :note note1
+  :value "Hello" })
+
+(add-record! :fields {
+  :field-type question-field-type
+  :note note2 
+  :value "おはようございます" })
+
+(add-record! :fields {
+  :field-type answer-field-type
+  :note note2
+  :value "Good morning" })
+
+(def default-card-type
+  (add-record! :card-types {
+    :name "Default Card"
+    :note-type default-note-type }))
+
+(def front-side
+  (add-record! :sides {
+    :name "Front"
+    :display "{{Question}}"
+    :card-type default-card-type}))
+
+(def back-side
+  (add-record! :sides {
+    :name "Back"
+    :display "{{Answer}}"
+    :card-type default-card-type}))
+
+(def deck1 (add-record! :decks { :name "日本語" }))
+
+(add-record! :cards {
+  :card-type default-card-type
+  :note note1
+  :deck deck1
+  :status :new
+  :last-review "3-10-2018" })
+
+(add-record! :cards {
+  :card-type default-card-type
+  :note note2
+  :deck deck1
+  :status :new
+  :last-review "3-10-2018" })
+
+;; -- Query Helpers -----------------------------------------------------------
+;;
+;; These are a series of functions that can be used to filter and find specific
+;; records in the database.
+;;
+;; By convention, the `find` function return a single result, whereas the
+;; `where` methods filter the hash.
+
 
 (defn where [key matcher collection]
   (->> collection
-       (filter #(-> % get-record key (= matcher)))
-       (into {})))
+       (filter #(-> % second key (= matcher))) ;; `second` is used to grab
+       (into {})))                             ;; the record. [:id { .. }]
 
+(defn where-id [keys collection]
+  (select-keys collection keys))
+
+;; TODO: find already exists
 (defn find [key matcher collection]
-  (->> collection
-       (filter #(-> % get-record key (= matcher)))
+  (->> collection vals
+       (filter #(-> % key (= matcher)))
        first))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;  ACTION DISPATCH
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; -- Persistence -------------------------------------------------------------
+;;
+;; For now the the entire application state is saved to LocalStorage after each
+;; event. In the future we might selectively store only certain parts of the
+;; state that gets stored. For instance, we may decide to not store the UI state.
 
+;(def local-storage-key "cljs-app")
+
+;(defn state->local-storage []
+  ;(.setItem js/localStorage local-storage-key (prn-str @state)))
+
+;(defn local-storage->state []
+  ;(reset! @state (some->> (.getItem js/localStorage local-storage-key)
+                          ;edn/read-string)))
+  
+
+;; -- Action Dispatch ---------------------------------------------------------
+;;
+;; The `dispatch` function recieves incoming actions and performs pre- and post-
+;; tasks before delegating to the corresponding event handler.
+
+(defn validate-state [] (js/console.log "Validating state."))
 (defn save-state [] (js/console.log "Saving state."))
 
 ;; TODO: Create UI actions and DB actions.
 ;; DB Actions persist to local storage, whereas UI actions do not.
 (defn dispatch [action]
-  (swap! state update-in [:actions] conj action)
   (handle action)
+  (swap! state update-in [:actions] conj action)
+  (validate-state)
   (save-state))
 
-(defn action-name [action] (first action))
+;; -- Event Handlers ----------------------------------------------------------
+;;
+;; Event handlers are passed an action, which is a vector of shape
+;; [:action-name arg & args]
+;; The multi method dispatches on the `first` element of the vector, namely
+;; the action-name.
 
-(defmulti  handle action-name)
+(defmulti  handle first) ;; Tests the first
 
 (defmethod handle :select-deck [[_ deck-id]]
   (swap! state assoc-in [:ui :workspace] [:deck deck-id]))
@@ -128,33 +210,32 @@
   (swap! state assoc-in [:ui :workspace] [:note-type note-type-id]))
 
 (defmethod handle :select-study [[_ deck-id]]
-  (swap! state assoc-in [:ui :workspace] [:study deck-id]))
+  (reset! ui-workspace [:study deck-id]))
 
 (defmethod handle :learn-card [[_ card-id]]
   (swap! state assoc-in [:db :cards card-id :status] :to-review))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;  COMPONENTS
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; -- View Components ---------------------------------------------------------
 
-(defn side-bar-deck-item [deck-id]
-  [:div {:on-click #(dispatch [:select-deck deck-id])}
-    (-> @state :db :decks deck-id :name)])
+; TODO: Visual indicator when components reload. Useful for optimizations.
+(defn side-bar-deck-item [deck]
+  [:div {:on-click #(dispatch [:select-deck (deck :id)])}
+    (deck :name)])
 
-(defn side-bar-note-type-item [note-type-id]
-  [:div {:on-click #(dispatch [:select-note-type note-type-id])}
-    (-> @state :db :note-types note-type-id :name)])
+(defn side-bar-note-type-item [note-type]
+  [:div {:on-click #(dispatch [:select-note-type (note-type :id)])}
+    (note-type :name)])
 
 (defn side-bar []
-  (let [all-deck-ids      (-> @state :db :decks      keys)
-        all-note-type-ids (-> @state :db :note-types keys)]
+  (let [decks      (-> @db-decks      vals)
+        note-types (-> @db-note-types vals)]
 
   [:div {:style styles/side-bar}
     [:h4 "Notes"]
-    (for [_ all-note-type-ids] ^{:key _} [side-bar-note-type-item _])
+    (for [_ note-types] ^{:key _} [side-bar-note-type-item _])
 
     [:h4 "Decks"]
-    (for [_ all-deck-ids] ^{:key _} [side-bar-deck-item _])]))
+    (for [_ decks] ^{:key _} [side-bar-deck-item _])]))
 
 (defn workspace [[workspace-name & args]]
   [:div {:style styles/workspace}
@@ -169,7 +250,7 @@
     "Welcome to the workspace!"])
 
 (defn workspace-note-type [note-type-id]
-  (let [note-type  (->> @state :db :note-types note-type-id)]
+  (let [note-type  (->> @db-note-types note-type-id)]
 
   [:div {:style styles/workspace-content} 
     [:p "Let's look at some notes for " (note-type :name) "!"]]))
@@ -178,13 +259,10 @@
 ;; TODO: Option to draw on the "front" side of a card
 
 (defn workspace-study [deck-id]
-  (let [deck  (->> @state :db :decks deck-id)
+  (let [deck  (->> @db-decks deck-id)
         ;; TODO: Filter by due today.
-        ;; TODO: ✔︎ Filter by new.
-        cards (->> @state :db :cards (where :deck deck-id)
-                                     (where :status :new))]
-
-  (js/console.log "Re-render workspace-study")
+        cards (->> @db-cards (where :deck deck-id)
+                             (where :status :new) vals)]
 
   [:div {:style styles/workspace-content} 
     (if (= 0 (count cards))
@@ -192,39 +270,35 @@
       [:div
         [:p "Now studying " (deck :name) "!"]
         [:p "Studying " (count cards) " cards."]
-        [:p "First card ID " (-> cards vals first :id)]
         [study-card (first cards)]
       ]
     )
   ]))
 
-(defn study-card [[card-id card]]
+(defn study-card [card]
   (let [current-side (r/atom 0)]
-    (fn [[card-id card]]
-      (let [card-type   (->> @state :db :card-types ((card :card-type)))
-            fields      (->> @state :db :fields (where :note (card :note)))
-            sides       (->  @state :db :sides (select-keys (card-type :sides)))
-            last-slide? (= (inc @current-side) (count sides))]
 
-      [:div
-        [:input {:type "button"
-                 :value "Next"
-                 :on-click (if last-slide?
-                             #(do (dispatch [:learn-card card-id])
-                                  (reset! current-side 0))
-                             #(swap! current-side inc)) } ]
-        [:div "Card: " (card :id)]
-        [:div "Study Card - Side: " (-> sides vals (nth @current-side) :display)]
-      ]
-      )
-    )
-  )
-)
+  (fn [card]
+    (let [card-type   (->> @db-card-types ((card :card-type)))
+          fields      (->> @db-fields (where :note (card :note)))
+          sides       (->> @db-sides (where :card-type (card :card-type)) vals)
+          last-slide? (= (inc @current-side) (count sides))]
+
+    [:div
+      [:input {:type "button"
+               :value "Next"
+               :on-click (if last-slide?
+                           #(do (dispatch [:learn-card (card :id)])
+                                (reset! current-side 0))
+                           #(swap! current-side inc)) } ]
+      [:div "Card: " (card :id)]
+      [:div "Study Card - Side: " (-> sides (nth @current-side) :display)]]))))
 
 
 (defn workspace-deck [deck-id]
-  (let [deck  (->> @state :db :decks deck-id)
-        cards (->> @state :db :cards (where :deck deck-id))]
+  (let [deck  (->> @db-decks deck-id)
+        cards (->> @db-cards (where :deck deck-id))]
+
   [:div {:style styles/workspace-content} 
     [:input {:type "button"
              :value "Study"
@@ -232,49 +306,46 @@
     [:h2 (deck :name)]
     [card-table cards] ]))
 
-;; Wishlist
-;; (columns % :th)
-;; @state -> (~> :db :card-types (card :card-types))
-
 (defn card-table [cards]
   (let [table-columns [:preview :note-type :card-type :status :due]]
+
+  (js/console.log cards)
 
   [:table
     [:thead [:tr (for [_ table-columns] [:th _])]]
     [:tbody
       (for [[id card] cards]
         ^{:key id} [card-table-row table-columns card]) ]]))
+        ;^{:key id} [:p id]) ]]))
 
 (defn card-table-row [visible-columns card]
-  (let [db              (@state :db)
-        card-type       (->> db :card-types ((card :card-type)))
-        note            (->> db :notes      ((card :note)))
-        note-type       (->> db :note-types ((note :note-type)))
-        fields          (->> db :fields      (where :note (card :note)))
-        ;field-types     (->> db :field-types (where :note-type (note :note-type)))
-        field-types     (-> db :field-types
-                            (select-keys (note-type :field-types)))
-        [front-id _]    (->> field-types (find :name "Front"))
-        [_ front-field] (->> fields (find :field-type front-id))]
+  (let [card-type      (->> @db-card-types ((card :card-type)))
+        note           (->> @db-notes      ((card :note)))
+        note-type      (->> @db-note-types ((note :note-type)))
+        fields         (->> @db-fields      (where :note (card :note)))
+        field-types    (->> @db-field-types (where :note-type (note :note-type)))
+        ;; TODO: This is broken.
+        ;field-types    (->> @db-field-types (where-id (note-type :field-types)))
+        ;{front-id :id} (->> field-types     (find :name "Front"))
+        ;front-field    (->> fields          (find :field-type front-id))
+        ]
   
   [:tr
     (for [col visible-columns]
       [:td {:style styles/table-td}
-           (case col
-             :preview   (front-field :value)
-             :note-type (note-type :name)
-             :card-type (card-type :name)
-             :status    (card :status)
-             :due       (card :last-review))])]))
+        (case col
+          ; TODO: Maybe just render the "Front" side instead.
+          ;:preview   (front-field :value)
+          :preview   "Not Preview"
+          :note-type (note-type :name)
+          :card-type (card-type :name)
+          :status    (card :status)
+          :due       (card :last-review))])]))
 
 (defn app []
   [:div {:style styles/app}
     [side-bar]
-    [workspace (-> @state :ui :workspace)]])
+    [workspace @ui-workspace]])
 
-(r/render [app]
-          (js/document.getElementById "app"))
+(r/render [app] (js/document.getElementById "app"))
 
-;(.render js/ReactDOM
-  ;(.createElement js/React "h2" nil "Hello, React!")
-  ;(.getElementById js/document "app"))
