a50d08382513cc4d44b1e51a49e3355b06a45fa2
Matthew Steedman <steedman87@gmail.com>
2018-06-27 08:23:01 -0400

    Prepend media path to media field values

--- src/app/views/side-bar/import.cljs
+++ src/app/views/side-bar/import.cljs
@@ -4,8 +4,14 @@
             [clojure.set  :refer [rename-keys]]
             [cljs.reader  :as edn]))
 
+(def electron (js/require "electron"))
 (def fs (js/require "fs"))
+(def path (js/require "path"))
 
+(def user-data (-> electron .-remote .-app (.getPath "userData")))
+(def user-data-media (.join path user-data "media"))
+
+;; TODO: Run this through the spec
 (defn import-deck []
   [:input {:name "import"
            :type "file"
@@ -24,6 +30,10 @@
                               {:name   "Imported Deck"
                                :template ""})
 
+                   save-path (.join path user-data-media (name new-deck))
+
+                   media-keys [:audio :screenshot]
+
                    new-edn (update-in edn [:fields] (fn [fields]
                      (as-> fields f
                        (rename-keys f new-field-keys)
@@ -36,8 +46,14 @@
                    newer-edn (update-in new-edn [:cards] (fn [cards]
                       (map
                         (fn [card]
+                          (js/console.log save-path)
                           (as-> card c
                             (update-in c [:fields] rename-keys new-field-keys)
+
+                            (update-in c [:fields (new-field-keys :audio)] 
+                                       #(js/encodeURI (.join path save-path %)))
+                            (update-in c [:fields (new-field-keys :screenshot)] 
+                                       #(js/encodeURI (.join path save-path %)))
                             (assoc c :deck-id new-deck
                                      :learning? true
                                      :reviews [])
