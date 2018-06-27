0322ae541668b62366839e924f3ce8a61b6caa85
Matthew Steedman <steedman87@gmail.com>
2018-06-27 08:22:31 -0400

    Allow line breaks in card markdown

--- src/app/views/card.cljs
+++ src/app/views/card.cljs
@@ -7,7 +7,7 @@
 (def all-cards (r/cursor state [:db :cards]))
 (def all-decks (r/cursor state [:db :decks]))
 (def all-fields (r/cursor state [:db :fields]))
-(def remarkable (js/Remarkable. #js {:html true}))
+(def remarkable (js/Remarkable. #js {:html true :breaks true}))
 
 ;; TODO: Rename this?
 (defn render-card [card template deck-fields]
